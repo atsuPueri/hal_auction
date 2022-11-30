b4deea786d1d8c8ff8e34593e04c71392e8e004f
yamadakai <101386443+yamadakai@users.noreply.github.com>
2022-11-30 13:57:59 +0900

    bootstrapに必要な値を渡した

--- Node/app.js
+++ Node/app.js
@@ -7,7 +7,7 @@ const http_socket = require('http').Server(app);
 const io_socket = require('socket.io')(http_socket);
 
 // 始めにする設定群
-require('./setting/bootstrap.js')(app);
+require('./settings/bootstrap.js')(app, express);
 
 // ルーティング
 require('./settings/web.js')(app);
--- Node/settings/bootstrap.js
+++ Node/settings/bootstrap.js
@@ -1,14 +1,14 @@
-module.exports = function(app) {
+module.exports = function(app, express) {
     
     // ビューエンジンをEJSに設定
     app.set('view engine', 'ejs');
-    app.use(express.static(__dirname + '/../../public/', {
+    app.use(express.static(__dirname + '/../../public/tmp', {
         index: false
     }));
 
-    app.use(express.static(__dirname + '/public', {
-        index: false
-    }));
+    // app.use(express.static(__dirname + '/public', {
+    //     index: false
+    // }));
 
     app.use(express.json());
     app.use(express.urlencoded({
