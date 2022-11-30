ba985b66691f4a8fd5eb98e2ce8ce65b6b6e2425
yamadakai <101386443+yamadakai@users.noreply.github.com>
2022-11-30 15:48:22 +0900

    ルーティング追記

--- Node/settings/web.js
+++ Node/settings/web.js
@@ -1,6 +1,29 @@
 module.exports = function(app) {
     app.get('/', (request, response) => {
-        response.render('index.ejs');
+        response.render('top');
     });
-    
+
+    app.get('/top', (request, response) => {
+        response.render('top');
+    })
+
+    app.get('/carentry', (request, response) => {
+        response.render('carentry');
+    });
+
+    app.get('/user', (request, response) => {
+        response.render('user');
+    });
+
+    app.get('/schedule', (request, response) => {
+        response.render('schedule');
+    });
+
+    app.get('/exhibit', (request, response) => {
+        response.render('exhibit');
+    });
+
+    app.get('/auction', (request, response) => {
+        response.render('auction');
+    });
 }
