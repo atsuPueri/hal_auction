088a038d3128a41c3f19ce05792df57489f7f99e
atsuPueri <yamahata.atsushi.as01@gmail.com>
2022-11-30 13:37:43 +0900

    bootstrapのviewを変更

--- Node/settings/bootstrap.js
+++ Node/settings/bootstrap.js
@@ -2,7 +2,7 @@ module.exports = function(app) {
     
     // ビューエンジンをEJSに設定
     app.set('view engine', 'ejs');
-    app.use(express.static(__dirname + '/../../public/', {
+    app.use(express.static(__dirname + '/../../public/tmp', {
         index: false
     }));
 
