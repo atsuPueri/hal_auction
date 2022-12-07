f1ac12e9c92b061d6ac0b75de9e025cdc08f82ae
atsuPueri <yamahata.atsushi.as01@gmail.com>
2022-12-07 13:38:27 +0900

    Update web.js

--- Node/settings/web.js
+++ Node/settings/web.js
@@ -3,12 +3,11 @@ module.exports = function(app) {
     const php = require('../util/php.js');
 
     app.get('/', (request, response) => {
-
-        php("/test", (m) => {
-            console.log(m);
-        });
         
         response.render('top.ejs');
     });
     
+    app.get('/schedule', (request, response) => {
+        response.render('schedule');
+    });
 }
