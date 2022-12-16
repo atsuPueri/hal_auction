dc9aed3475b5b7c385021fa4d40b08cbd5ec1850
kouki1024 <105490477+Narbekouki@users.noreply.github.com>
2022-12-16 18:53:49 +0900

    slider

---
+++ public/js/slider.js
@@ -0,0 +1,11 @@
+$('.slider').slick({
+    dots: true,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    prevArrow: '<div class="slick-prev"></div>',//矢印部分PreviewのHTMLを変更
+    nextArrow: '<div class="slick-next"></div>',//矢印部分NextのHTMLを変更
+    dots: true,//下部ドットナビゲーションの表示
+  });
+      
