c54415aa2b2fa920a55ad8761db78e18838a1ee1
chocomint3 <105490347+chocomint3@users.noreply.github.com>
2022-12-29 23:02:02 +0900

    ejs

--- Node/settings/web.js
+++ Node/settings/web.js
@@ -3,12 +3,7 @@ module.exports = function(app) {
     const php = require('../util/php.js');
 
     app.get('/', (request, response) => {
-
-        php("/test", (m) => {
-            console.log(m);
-        });
-        
-        response.render('top.ejs');
+        response.render('inform.ejs');
     });
     
 }
