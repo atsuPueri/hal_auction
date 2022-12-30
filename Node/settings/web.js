190cc0af59e0f59cae9cfc066a3fc818f4df1f31
kouki1024 <105490477+Narbekouki@users.noreply.github.com>
2022-12-30 23:38:52 +0900

    web.js パスを通した

--- Node/settings/web.js
+++ Node/settings/web.js
@@ -3,7 +3,22 @@ module.exports = function(app) {
     const php = require('../util/php.js');
 
     app.get('/', (request, response) => {
-        response.render('inform.ejs');
+        response.render('user_top.ejs');
     });
     
+    app.get('/mypage', (request, response) => {
+        response.render('mypage');
+    });
+
+    app.get('/schedule', (request, response) => {
+        response.render('schedule');
+    });
+
+    app.get('/exhibit', (request, response) => {
+        response.render('exhibit');
+    });
+
+    app.get('/auction', (request, response) => {
+        response.render('auction');
+    });
 }
